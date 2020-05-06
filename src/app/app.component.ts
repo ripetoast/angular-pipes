e60f099edead4c8e9003625de7cf6525791f2ff6
Michael <michael_lau@live.co.uk>
2020-05-06 18:07:41 +0100

    ADD: More pipe usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
+  date: string;
+  amount: number;
+  height: number;
+
+  onHeightChange(value: string){
+    this.height = parseFloat(value);
+  }
 
   onNameChange(value: string){
     this.name = value;
+  }
+
+  onDateChange(value: string){
+    this.date = value;
+  }
+
+  onAmountChange(value: string){
+    this.amount = parseFloat(value);
   }
 }
